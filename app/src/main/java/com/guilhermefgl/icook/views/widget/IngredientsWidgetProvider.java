cec2d33b9d5d23e8f548841e59b5469f2009a7a5
guilhermefgl <Leonardo.souza@Montreal.com.br>
2018-04-03 13:50:57 -0300

    refactoring widget

--- app/src/main/java/com/guilhermefgl/icook/views/widget/IngredientsWidgetProvider.java
+++ app/src/main/java/com/guilhermefgl/icook/views/widget/IngredientsWidgetProvider.java
@@ -13,6 +13,7 @@ import com.guilhermefgl.icook.R;
 import com.guilhermefgl.icook.models.entitys.Recipe;
 import com.guilhermefgl.icook.services.tasks.SelectCurrentWidgetRecipe;
 import com.guilhermefgl.icook.views.recipe.RecipeActivity;
+import com.guilhermefgl.icook.views.splash.SplashActivity;
 
 public class IngredientsWidgetProvider extends AppWidgetProvider {
 
@@ -47,17 +48,23 @@ public class IngredientsWidgetProvider extends AppWidgetProvider {
         views.setEmptyView(R.id.widget_recipe_list, R.id.widget_empty_layout);
         views.setRemoteAdapter(R.id.widget_recipe_list,
                 new Intent(context, ListWidgetFactoryService.class));
+
         if (recipe != null) {
             views.setTextViewText(R.id.widget_title, recipe.getName());
-        }
 
-        Bundle extras = new Bundle();
-        extras.putParcelable(RecipeActivity.BUNDLE_RECIPE, recipe);
-        Intent appIntent = new Intent(context, RecipeActivity.class);
-        appIntent.putExtras(extras);
-        PendingIntent appPendingIntent = PendingIntent
-                .getActivity(context, INTENT_REQUEST_CODE, appIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        views.setPendingIntentTemplate(R.id.widget_recipe_layout, appPendingIntent);
+            Bundle extras = new Bundle();
+            extras.putParcelable(RecipeActivity.BUNDLE_RECIPE, recipe);
+            Intent appIntent = new Intent(context, RecipeActivity.class);
+            appIntent.putExtras(extras);
+            PendingIntent appPendingIntent = PendingIntent
+                    .getActivity(context, INTENT_REQUEST_CODE, appIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setOnClickPendingIntent(R.id.widget_recipe_layout, appPendingIntent);
+        } else {
+            Intent appIntent = new Intent(context, SplashActivity.class);
+            PendingIntent appPendingIntent = PendingIntent
+                    .getActivity(context, INTENT_REQUEST_CODE, appIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+            views.setOnClickPendingIntent(R.id.widget_empty_layout, appPendingIntent);
+        }
 
         appWidgetManager.updateAppWidget(appWidgetId, views);
         appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_recipe_list);
